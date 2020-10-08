d4e9dfbedb0c046b8d9f7cd40c4ca2b87458a9c8
Joolee Chin <jooleechin@gmail.com>
2020-10-08 15:23:15 -0700

    finished functional of checkout page, but still needs styling

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Subtotal from './Subtotal'
 import "./Checkout.css"
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+      const [{basket}, dispatch] = useStateValue()
+
       return (
             <div className="checkout">
                   <div className="checkout__left">
@@ -13,9 +17,18 @@ function Checkout() {
                         />
                         <div className="">
                               <h2 className="checkout__title">Your Shopping Cart</h2>
+                              {basket.map(item => (
+                                    <CheckoutProduct
+                                          id={item.id}
+                                          title={item.title}
+                                          image={item.image}
+                                          price={item.price}
+                                          rating={item.rating}
+                                    />
+                              ))}
                         </div>
                   </div>
-
+ 
                   <div className="checkout__right">
                         <Subtotal />
                   </div>
