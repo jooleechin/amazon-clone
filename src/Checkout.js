51319d9960830e4351e600e04008b37f87ce0813
Joolee Chin <jooleechin@gmail.com>
2020-10-08 16:33:20 -0700

    finished build without stripe, will add stripe tomorrow

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Subtotal from './Subtotal'
 import "./Checkout.css"
+// import FlipMove from 'react-flip-move'
 import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
-      const [{basket}, dispatch] = useStateValue()
+      const [{ basket, user }, dispatch] = useStateValue()
 
       return (
             <div className="checkout">
@@ -15,17 +16,21 @@ function Checkout() {
                               src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                               alt="checkout banner"
                         />
-                        <div className="">
+                        <div>
+                              <h3>{user?.email}</h3>
                               <h2 className="checkout__title">Your Shopping Cart</h2>
-                              {basket.map(item => (
-                                    <CheckoutProduct
-                                          id={item.id}
-                                          title={item.title}
-                                          image={item.image}
-                                          price={item.price}
-                                          rating={item.rating}
-                                    />
-                              ))}
+                              {/* <FlipMove> */}
+                                    {basket.map(item => (
+                                          <CheckoutProduct
+                                                key={item.id}
+                                                id={item.id}
+                                                title={item.title}
+                                                image={item.image}
+                                                price={item.price}
+                                                rating={item.rating}
+                                          />
+                                    ))}
+                              {/* </FlipMove> */}
                         </div>
                   </div>
  
