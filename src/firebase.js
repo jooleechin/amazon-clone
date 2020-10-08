52cb750b10441288fac5774e5689a672160dfe24
Joolee Chin <jooleechin@gmail.com>
2020-10-08 13:05:20 -0700

    added the header and home page

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+      apiKey: "AIzaSyBZqjVbctElegn88WefdLy5N9qx-WrHr8Q",
+      authDomain: "clone-19087.firebaseapp.com",
+      databaseURL: "https://clone-19087.firebaseio.com",
+      projectId: "clone-19087",
+      storageBucket: "clone-19087.appspot.com",
+      messagingSenderId: "84014886422",
+      appId: "1:84014886422:web:0d0f65cdffe1ead1264f37",
+      measurementId: "G-HHH87PPLDK"
+    };
